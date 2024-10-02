8cd9b0c9605875fb637e20dc7a8fa9d9719353f1
ttduycapricorn <ttduycapricorn@gmail.com>
2024-10-02 17:09:22 +0700

    update

--- src/components/maps/MapComponent.jsx
+++ src/components/maps/MapComponent.jsx
@@ -1,27 +1,42 @@
-import { MapContainer, WMSTileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function MapComponent() {
+    const [geoJsonData, setGeoJsonData] = useState(null);
+
+    useEffect(() => {
+        // Replace with your GeoServer API URL
+        const geoJsonUrl = 'http://localhost:8082/api';
+
+        // Fetch GeoJSON data from backend
+        axios
+            .get(geoJsonUrl)
+            .then((response) => {
+                const geoJson = response.data.data;
+                console.log('GeoJSON Data:', geoJson);
+                setGeoJsonData(geoJson);
+            })
+            .catch((error) => {
+                console.error('Error fetching GeoJSON from backend:', error);
+            });
+    }, []);
+
     return (
         <MapContainer
-            center={[9.675, 105.9043]}
+            center={[9.675, 105.9043]} // Change the coordinates to fit your data
             zoom={13}
-            scrollWheelZoom={false}
+            scrollWheelZoom={true}
             style={{ height: '600px', width: '100%', zIndex: '0' }}
         >
-            {/* <TileLayer
+            <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://mt1.google.com/vt/lyrs=r&x={x}&y={y}&z={z}"
-            /> */}
-            <WMSTileLayer
-                url="http://192.168.64.5:8080/geoserver/ne/wms"
-                layers="ne:boundary_lines"
-                format="image/png"
-                transparent={true}
-                attribution="&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
             />
 
-            {/* <GeoJSON data={geoData.features} style={CountryStyleList} /> */}
+            {/* Conditionally render GeoJSON layer once data is fetched */}
+            {geoJsonData && <GeoJSON data={geoJsonData} />}
         </MapContainer>
     );
 }
