760449e17c212549d91a7227b26d70a801dfc86c
minhkhacode <nkha2888@gmail.com>
2024-10-17 13:56:48 +0700

    update

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -10,72 +10,73 @@ function Test() {
     const dispatch = useDispatch();
     const { layer, compareLayer } = useSelector((state) => state.layer);
 
-    // const [geoJsonData, setGeoJsonData] = useState(null);
-
-    // useEffect(() => {
-    //     dispatch(fetchLayerGeoJson());
-    // }, [dispatch]);
-
-    // useEffect(() => {
-    //     axios
-    //         .get(`/geoserver/wfs`, {
-    //             params: {
-    //                 service: 'WFS',
-    //                 version: '1.0.0',
-    //                 request: 'GetFeature',
-    //                 typeName: 'minhkha:thuanhoa_tkdd2022',
-    //                 outputFormat: 'application/json',
-    //                 srsName: 'EPSG:4326',
-    //             },
-    //         })
-    //         .then((data) => {
-    //             console.log(data.data);
-
-    //             setGeoJsonData(data.data);
-    //         });
-    // }, []);
-    const [NN, setNN] = useState(null);
-    const [PNN, setPNN] = useState(null);
-    const [TQ, setTQ] = useState(null);
-
-    console.log(compareLayer);
+    const [geoJsonData, setGeoJsonData] = useState(null);
 
     useEffect(() => {
-        axios.get('/NN.geojson').then((data) => {
-            setNN(data.data);
-        });
+        dispatch(fetchLayerGeoJson());
+    }, [dispatch]);
 
-        axios.get('/PNN.geojson').then((data) => {
-            setPNN(data.data);
-        });
+    useEffect(() => {
+        axios
+            .get(`/geoserver/wfs`, {
+                params: {
+                    service: 'WFS',
+                    version: '1.0.0',
+                    request: 'GetFeature',
+                    typeName: 'minhkha:thuanhoa_tkdd2022',
+                    outputFormat: 'application/json',
+                    srsName: 'EPSG:4326',
+                },
+            })
+            .then((data) => {
+                console.log(data.data);
 
-        axios.get('/TQ.geojson').then((data) => {
-            setTQ(data.data);
-        });
+                setGeoJsonData(data.data);
+            });
     }, []);
 
-    const handleToggleNN = () => {
-        dispatch(toggleNN({ ...NN }));
-        console.log(compareLayer);
-    };
+    // const [NN, setNN] = useState(null);
+    // const [PNN, setPNN] = useState(null);
+    // const [TQ, setTQ] = useState(null);
 
-    const handleTogglePNN = () => {
-        dispatch(togglePNN({ ...PNN }));
-        console.log(compareLayer);
-    };
+    // console.log(compareLayer);
 
-    const handleToggleTQ = () => {
-        dispatch(toggleTQ({ ...TQ }));
-        console.log(compareLayer);
-    };
+    // useEffect(() => {
+    //     axios.get('/NN.geojson').then((data) => {
+    //         setNN(data.data);
+    //     });
+
+    //     axios.get('/PNN.geojson').then((data) => {
+    //         setPNN(data.data);
+    //     });
+
+    //     axios.get('/TQ.geojson').then((data) => {
+    //         setTQ(data.data);
+    //     });
+    // }, []);
+
+    // const handleToggleNN = () => {
+    //     dispatch(toggleNN({ ...NN }));
+    //     console.log(compareLayer);
+    // };
+
+    // const handleTogglePNN = () => {
+    //     dispatch(togglePNN({ ...PNN }));
+    //     console.log(compareLayer);
+    // };
+
+    // const handleToggleTQ = () => {
+    //     dispatch(toggleTQ({ ...TQ }));
+    //     console.log(compareLayer);
+    // };
 
     return (
         <div>
-            <input type="checkbox" onChange={handleToggleNN} />
+            {/* <input type="checkbox" onChange={handleToggleNN} />
             <input type="checkbox" onChange={handleTogglePNN} />
             <input type="checkbox" onChange={handleToggleTQ} />
-            <MapShapeFile getJsonDataList={Object.values(compareLayer)} />
-            {/* <MapContainer
+            <MapShapeFile getJsonDataList={Object.values(compareLayer)} /> */}
+            <MapContainer
                 center={[9.680258, 105.905196]}
                 zoom={13}
                 scrollWheelZoom={true}
@@ -86,7 +87,7 @@ function Test() {
                     url="https://mt1.google.com/vt/lyrs=r&x={x}&y={y}&z={z}"
                 />
                 {geoJsonData && <GeoJSON key="my-geojson" data={geoJsonData.features} />}
-            </MapContainer> */}
+            </MapContainer>
         </div>
     );
 }
